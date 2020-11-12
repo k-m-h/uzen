2490ed89ff90c860583c90a1b3df6d5c4efd449a
poetthefrog <kenth1@uci.edu>
2020-11-12 00:15:59 -0800

    start logo component & clena page

---
+++ components/Logo.js
@@ -0,0 +1,11 @@
+import styles from './Logo.module.css'
+
+export default function Logo() {
+  return (
+    <>
+      <logo className={styles.logo}>
+        <img src="japanese_cuisine_logo_by_m1k3y_grey_d781p8x.jpg" alt="Uzen Logo" className={styles.logo} />
+      </logo>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Logo from '@components/Logo'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Uzen Resaurant</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
