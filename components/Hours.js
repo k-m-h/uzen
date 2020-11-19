90905962e09a8129dddd7fc23f7c05ae5e074e9a
poetthefrog <kenth1@uci.edu>
2020-11-19 08:25:21 -0800

    time change

--- components/Hours.js
+++ components/Hours.js
@@ -5,11 +5,12 @@ export default function Hours() {
     <section className="hours">
       <div>
         <h4>Take-out Hours</h4>
-        <p><span>Monday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-        <p><span>Tuesday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-        <p><span>Wednesday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-        <p><span>Thursday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-        <p><span>Saturday</span>	<span>5:30–10PM</span></p>
+        <p><span>Monday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+        <p><span>Tuesday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+        <p><span>Wednesday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+        <p><span>Thursday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+        <p><span>Friday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+        <p><span>Saturday</span>	<span>5:00PM–8:00PM</span></p>
         <p><span>Sunday</span>	<span>Closed</span></p>
       </div>
       <div>
--- pages/index.js
+++ pages/index.js
@@ -17,18 +17,19 @@ export default function Home() {
         subTitle="Japanese Cuisine"
       />
       <section>
-        <div className="hours">
-          <h4>Take-out Hours</h4>
-          <p><span>Monday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-          <p><span>Tuesday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-          <p><span>Wednesday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-          <p><span>Thursday</span>	<span>11:00AM – 2:00PM, 5:00PM – 8:000PM</span></p>
-          <p><span>Saturday</span>	<span>5:30–10PM</span></p>
-          <p><span>Sunday</span>	<span>Closed</span></p>
-        </div>
-        <div>
+        <div className="hoursDinein">
           <h4>Dine-in Hours</h4>
           <span>Currently Unavailable</span>
+        </div>
+        <div className="hoursTakeout">
+          <h4>Take-out Hours</h4>
+          <p><span>Monday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+          <p><span>Tuesday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+          <p><span>Wednesday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+          <p><span>Thursday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+          <p><span>Friday</span>	<span>11:00AM – 1:00PM, 5:00PM – 8:000PM</span></p>
+          <p><span>Saturday</span>	<span>5:00PM–8:00PM</span></p>
+          <p><span>Sunday</span>	<span>Closed</span></p>
         </div>
         <div className="holidays">
           <h4>Upcoming Holiday Closures</h4>
