f4cd53f87c2872e367f1d07a1a1047e5ede82785
m1k3y <m1k3y@gHost.local>
2023-08-05 22:49:10 -0700

    update hours

--- components/Hours.js
+++ components/Hours.js
@@ -4,17 +4,16 @@ export default function Hours({dineIn, takeOut}) {
   const unavailableContent = <span>Currently Unavailable due to COVID-19</span>;
   const dineInContent = dineIn ?
     <>
-      <p>Currently operating at limited capacity</p>
-      <p><span>Mon-Fri</span>	<span>11:00AM–1:00PM, 5:00PM–8:30PM</span></p>
-      <p><span>Sat</span>	<span>5:00PM–8:30PM</span></p>
+      <p><span>Mon-Fri</span>	<span>11:00AM–1:00PM, 5:30PM–9:00PM</span></p>
+      <p><span>Sat</span>	<span>5:30PM–9:00PM</span></p>
       <p><span>Sun</span>	<span>Closed</span></p>
     </> :
     unavailableContent;
 
   const takeOutContent = takeOut ?
     <>
-      <p><span>Mon-Fri</span>	<span>11:00AM–1:00PM, 5:00PM–8:30PM</span></p>
-      <p><span>Sat</span>	<span>5:00PM–8:30PM</span></p>
+      <p><span>Mon-Fri</span>	<span>11:00AM–1:00PM, 5:30PM–9:00PM</span></p>
+      <p><span>Sat</span>	<span>5:30PM–9:00PM</span></p>
       <p><span>Sun</span>	<span>Closed</span></p>
     </> :
     unavailableContent;
